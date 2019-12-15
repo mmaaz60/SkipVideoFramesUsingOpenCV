1370d181bc28f9d8cf9073bc7971b6752229cbd7
Muhammad Maaz <mmaaz60@gmail.com>
2019-12-15 19:36:00 +0500

    Initial Commit

---
+++ skip_frames_using_read.py
@@ -0,0 +1,33 @@
+import cv2
+import time
+
+video_name = "video/GOT.mp4"
+processing_fps = 5
+
+video = cv2.VideoCapture(video_name)
+video_fps = video.get(cv2.CAP_PROP_FPS)
+
+if video_fps > processing_fps:
+	skip_rate = round(video_fps/processing_fps)
+else:
+	skip_rate = 1
+
+frame_no = 0
+processed_frame_count = 0
+
+start = time.time()
+while True:
+	ret, frame = video.read()
+	if not ret:
+		break
+	frame_no += 1
+	
+	if (frame_no % skip_rate == 0):
+		processed_frame_count += 1
+		frame = cv2.resize(frame, (1280,720))
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		cv2.imshow("Frame", frame)
+		cv2.waitKey(1)
+
+end = time.time()
+print("Processing FPS (.read()): " + str(processed_frame_count/(end - start)))
