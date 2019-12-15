f563f32390eab2695751bb1995c9dd76ee790829
Muhammad Maaz <mmaaz60@gmail.com>
2019-12-15 21:12:53 +0500

    Added command line arguments (video path and processing fps). Added comments to increase readability.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Skip Video Frames using OpenCV
+
+In this repository, I will show you two methods to skip frames while processing a video in any video processing application. I will provide a comparison of two methods in terms of speed. So let's get started.
+
+### Skip Frames usin VideoCapture.read() meathod
+
+The script **"skip_frames_using_read.py"** implements this procedure of skiping frames while using VideoCapture.read() method of OpenCV.
--- skip_frames_using_grab.py
+++ skip_frames_using_grab.py
@@ -1,34 +1,61 @@
+import argparse
 import cv2
 import time
 
-video_name = "video/GOT.mp4"
-processing_fps = 5
 
-video = cv2.VideoCapture(video_name)
-video_fps = video.get(cv2.CAP_PROP_FPS)
-
-if video_fps > processing_fps:
-	skip_rate = round(video_fps/processing_fps)
-else:
-	skip_rate = 1
-
-frame_no = 0
-processed_frame_count = 0
-
-start = time.time()
-while True:
-	ret = video.grab()
-	if not ret:
-		break
-	frame_no += 1
+def parse_arguments():
+	ap = argparse.ArgumentParser()
 	
-	if (frame_no % skip_rate == 0):
-		processed_frame_count += 1
-		status, frame = video.retrieve()
-		frame = cv2.resize(frame, (1280,720))
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		cv2.imshow("Frame", frame)
-		cv2.waitKey(1)
+	ap.add_argument("-v", "--video", type=str, required=True, 
+					help="path to input video file")
+	ap.add_argument("-fps", "--processing_fps", type=int, required=True, 
+					help="number of processing frames per second")
+	args = vars(ap.parse_args())
+	
+	return args
 
-end = time.time()
-print("Processing FPS (.grab() & .retrieve()): " + str(processed_frame_count/(end - start)))
+
+if __name__ == '__main__':
+	# Parse command line arguments
+	args = parse_arguments()
+	
+	# Set local variables
+	video_name = args["video"]
+	processing_fps = args["processing_fps"]
+
+	# Initialize OpenCV video capture object and get video FPS
+	video = cv2.VideoCapture(video_name)
+	video_fps = video.get(cv2.CAP_PROP_FPS)
+
+	# Calculate and set the skip rate
+	if video_fps > processing_fps:
+		skip_rate = round(video_fps/processing_fps)
+	else:
+		skip_rate = 1
+
+	frame_no = 0  # Local variable to keep track of video frame number
+	processed_frame_count = 0  # Local variable to count total processing frames
+
+	start = time.time()  # Capture start time, this will be used for processing fps calculation
+	
+	while True:
+		ret = video.grab()
+		if not ret:
+			break  # Video ended
+		frame_no += 1
+		
+		if (frame_no % skip_rate == 0):  # Processing frame
+			processed_frame_count += 1
+			
+			status, frame = video.retrieve()  # Decode processing frame
+			
+			# Some image processing operations; resize, convert to gray scale and show frame
+			frame = cv2.resize(frame, (1280,720))
+			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			cv2.imshow("Frame", frame)
+			cv2.waitKey(1)  # Wait for 1 ms, this is to clearly see the frame on screen
+
+	end = time.time()  # Capture end time
+	
+	# Processing FPS = Total Processing Frames / Time to process
+	print("Processing FPS (.grab()): " + str(processed_frame_count/(end - start)))
