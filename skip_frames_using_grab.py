9e23e38222b249f26e560d507b18ef6e869fddf9
Muhammad Maaz <mmaaz60@gmail.com>
2019-12-16 23:46:57 +0500

    Added code to calculate the time to read, grab and retrieve frame.

--- skip_frames_using_grab.py
+++ skip_frames_using_grab.py
@@ -36,18 +36,26 @@ if __name__ == '__main__':
 	frame_no = 0  # Local variable to keep track of video frame number
 	processed_frame_count = 0  # Local variable to count total processing frames
 
+	total_grab_time = 0 # Local variable to save the total frame grab time
+	total_retrieve_time = 0 # Local variable to save the total frame retreive time
+
 	start = time.time()  # Capture start time, this will be used for processing fps calculation
 	
 	while True:
+		tmp = time.time()
 		ret = video.grab()
+		total_grab_time += (time.time() - tmp)
 		if not ret:
 			break  # Video ended
+		
 		frame_no += 1
 		
 		if (frame_no % skip_rate == 0):  # Processing frame
 			processed_frame_count += 1
 			
+			tmp = time.time()
 			status, frame = video.retrieve()  # Decode processing frame
+			total_retrieve_time += (time.time() - tmp)
 			
 			# Some image processing operations; resize, convert to gray scale and show frame
 			frame = cv2.resize(frame, (1280,720))
@@ -55,7 +63,15 @@ if __name__ == '__main__':
 			cv2.imshow("Frame", frame)
 			cv2.waitKey(1)  # Wait for 1 ms, this is to clearly see the frame on screen
 
+	video.release()
+	
 	end = time.time()  # Capture end time
+	
+	# Average frame grab time
+	print("Average frame grab time: " + str(1000*total_grab_time/frame_no) + " ms")
+	
+	# Average frame retrieve time
+	print("Average frame retrieve time: " + str(1000*total_retrieve_time/processed_frame_count) + " ms")
 	
 	# Processing time
 	print("Processing Time (.grab()): " + str(end - start) + " sec")
--- skip_frames_using_read.py
+++ skip_frames_using_read.py
@@ -36,12 +36,17 @@ if __name__ == '__main__':
 	frame_no = 0  # Local variable to keep track of video frame number
 	processed_frame_count = 0  # Local variable to count total processing frames
 
+	total_read_time = 0
+
 	start = time.time()  # Capture start time, this will be used for processing fps calculation
 	
 	while True:
+		tmp = time.time()
 		ret, frame = video.read()
+		total_read_time += (time.time() - tmp)
 		if not ret:
 			break  # Video ended
+		
 		frame_no += 1
 		
 		if (frame_no % skip_rate == 0):  # Processing frame
@@ -53,7 +58,12 @@ if __name__ == '__main__':
 			cv2.imshow("Frame", frame)
 			cv2.waitKey(1)  # Wait for 1 ms, this is to clearly see the frame on screen
 
+	video.release()
+	
 	end = time.time()  # Capture end time
+	
+	# Average frame read time
+	print("Average frame read time: " + str(1000*total_read_time/frame_no) + " ms")
 	
 	# Processing time
 	print("Processing Time (.read()): " + str(end - start) + " sec")
